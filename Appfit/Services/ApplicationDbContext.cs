63398460fc63ceb21ba785b368b3a46fe1eb30e1
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-06 14:10:37 +0000

    Add project files.

---
+++ Appfit/Services/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Appfit.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
