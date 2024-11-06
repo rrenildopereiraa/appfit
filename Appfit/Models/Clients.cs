c6ce9535a1723a9063be542f981910d9547473da
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-06 18:19:57 +0000

    app:
    - replace *-primary with *-dark color utilities
    - add logo

---
+++ Appfit/Models/Clients.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appfit.Models
+{
+    public class Clients
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Address { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = string.Empty;
+
+        public DateTime RegisteredAt { get; set; }
+    }
+}
