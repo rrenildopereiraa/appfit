e62856388e3ef35d11fbd959ac04f42a681e8114
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-07 16:40:34 +0000

    app: - add `AppfitBooking.cs` model - add `AppfitClass.cs` model - fix `Views\AppfitUser` - remove `Views\AppfitUser` folder directory

---
+++ Appfit/Models/AppfitBooking.cs
@@ -0,0 +1,16 @@
+﻿namespace Appfit.Models
+{
+    public class AppfitBooking
+    {
+        public int Id { get; set; }
+
+        public string? UserId { get; set; }
+        public AppfitUser? User { get; set; }
+
+        public int WorkoutClassId { get; set; }
+        public AppfitClass? WorkoutClass { get; set; }
+
+        public DateTime BookingDate { get; set; }
+        public bool isConfirmed { get; set; }
+    }
+}
---
+++ Appfit/Models/AppfitClass.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appfit.Models
+{
+    public class AppfitClass
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string Category { get; set; } = string.Empty;
+
+        public int GymId { get; set; }
+
+        public AppfitGym? Gym { get; set; }
+
+        public string? CoachId { get; set; }
+        public AppfitUser? Coach { get; set; }
+
+        public int MaxAttendees { get; set; } = 20;
+
+        public TimeSpan Duration { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
