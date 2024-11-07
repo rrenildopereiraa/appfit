09fe5c2eab0691916d1ea15b502de2f4ff2568b5
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-07 14:54:37 +0000

    add Non-nullable property

--- Appfit/Models/AppfitSubscription.cs
+++ Appfit/Models/AppfitSubscription.cs
@@ -6,7 +6,7 @@
         public string? UserId { get; set; }
         public AppfitUser? User { get; set; }
         public int GymId { get; set; }
-        public AppfitGym Gym { get; set; }
+        public AppfitGym? Gym { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public bool IsSubscribed { get; set; }
