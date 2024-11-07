52a612c857fc53d0179bd95dbde688a762169369
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-07 14:49:18 +0000

    app: - add `AppfitGym.cs` model - add `AppfitSubscription.cs` model

---
+++ Appfit/Models/AppfitGym.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appfit.Models
+{
+    public class AppfitGym
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string LocationName { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string Address { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+        public string OpeningHours { get; set; } = string.Empty;
+    }
+}
---
+++ Appfit/Models/AppfitSubscription.cs
@@ -0,0 +1,14 @@
+﻿namespace Appfit.Models
+{
+    public class AppfitSubscription
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public AppfitUser User { get; set; }
+        public int GymId { get; set; }
+        public AppfitGym Gym { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsSubscribed { get; set; }
+    }
+}
